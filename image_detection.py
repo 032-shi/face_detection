a06903863851d1fb666216c589ca768d9f2999ab
032-shi <0324omitsu@gmail.com>
2021-09-25 01:30:46 +0900

    [add]動画の顔検出プログラム追加

---
+++ image_detection.py
@@ -0,0 +1,10 @@
+import face_recognition
+import cv2
+image = face_recognition.load_image_file("3.jpg") #対象の画像ファイルを選択
+face_locations = face_recognition.face_locations(image, model = "cnn") #顔の位置座標を配列で取得、"model=cnn"で検出精度を上げる
+
+for (top, right, bottom, left) in face_locations: #顔の位置を枠線で囲う処理
+    cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+cv2.imwrite("sample.jpg", image) #顔の位置に枠線を描いた画像を保存する
--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 import face_recognition
 import cv2
-image = face_recognition.load_image_file("1.jpg") #対象の画像ファイルを選択
-face_locations = face_recognition.face_locations(image) #顔の位置座標を配列で取得
+import numpy as np
 
-for (top, right, bottom, left) in face_locations: #顔の位置を枠線で囲う処理
-    cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
 
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-cv2.imwrite("sample.jpg", image) #顔の位置に枠線を描いた画像を保存する
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture("1.mp4")
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("A.jpg")
+print(face_recognition.face_encodings(obama_image)[0])
+#obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
